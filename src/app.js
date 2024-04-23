307c54a17156290c9c0accb8681aa9d7a1c56687
Gospel011 <ugochukwugospeli111@gmail.com>
2024-04-23 21:12:54 +0100

    Finished implementing google sign in

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const querystring = require('querystring');
 const axios = require('axios');
 const util = require('util');
 const fs = require('fs');
-// const {promisify} = require('promissify');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 app.use(express.static(__dirname + '/public'));
 
@@ -16,6 +16,26 @@ const redirect = `${process.env.SERVER_ROOT_URI}/${redirectURI}`;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
+//! google auth endpoints
+/**
+ * This is the url for starting off the authentication process.
+ * Some query strings need to be appended for it to work.
+ */
+const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+/**
+ * This is the endpoint for accessing the user's info with the provided access_token.
+ * The access token needs to be appended for it to work.
+ */
+const userinfoUrl = `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=`;
+
+/**
+ * This is the endpoint for getting the users acccess_token, refresh_token and id_token.
+ */
+const tokensUrl = 'https://oauth2.googleapis.com/token';
+
+//!
+
 // function objectToQueryString(obj) {
 //   return Object.keys(obj)
 //     .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
@@ -23,8 +43,6 @@ const clientSecret = process.env.CLIENT_SECRET;
 // }
 
 const getGoogleAuthURL = () => {
-  const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
-
   console.log(`::: R E D I R E C T   U R I  ${redirect}`);
 
   const options = {
@@ -55,7 +73,7 @@ async function getTokens(code) {
    * Uses the code to get tokens
    * that can be used to fetch the user's profile
    */
-  const url = 'https://oauth2.googleapis.com/token';
+
   const values = {
     code,
     client_id: clientId,
@@ -68,55 +86,92 @@ async function getTokens(code) {
 
   var res;
 
-  //   try {
-  res = await axios.post(url, querystring.stringify(values), {
-    headers: {
-      'Content-Type': 'application/application/x-www-form-urlencoded',
-    },
-  }).then((data) => {
-    console.log("Data is", data);
-  }).catch((error) => {
-    console.log("Failed to get tokens")
-  });
+  try {
+    const response = await axios.post(
+      tokensUrl,
+      querystring.stringify(values),
+      {
+        headers: {
+          'Content-Type': 'application/application/x-www-form-urlencoded',
+        },
+      }
+    );
 
-//   console.log('success response is', res.data);
-
-  return res;
+    return response.data;
+  } catch (error) {
+    console.log('Failed to get auth token');
+  }
 }
 
-//*
-async function getAuthTokens(code) {
-  const tokenUrl = 'https://oauth2.googleapis.com/token';
-  const data = {
-    code: code,
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uri: redirectURI,
-    grant_type: 'authorization_code',
-  };
-
-  axios
-    .post(tokenUrl, querystring.stringify(data), {
+async function getGoogleUser({ access_token, id_token }) {
+  try {
+    const response = await axios.get(`${userinfoUrl}${access_token}`, {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${id_token}`,
       },
-    })
-    .then((response) => {
-      console.log('Access token:', response.data.access_token);
-    })
-    .catch((error) => {
-      console.error('Error:', error.response.data);
     });
+
+    return response.data;
+  } catch (error) {
+    console.error(`Failed to fetch user`);
+    console.log(error.message);
+  }
 }
+
+app.get('/auth', async (req, res, next) => {
+  const { access_token, id_token } = req.query;
+  // console.log(req.query);
+
+  const user = await getGoogleUser({ access_token, id_token });
+
+  if(!user) return res.set('Content-Type', 'text/html').send('<h1>An error occured</h1>');
+
+
+  console.log(`User is ${util.inspect(user)}`);
+  const jwt_secret = process.env.JWT_SECRET;
+
+  const token = jwt.sign(user, jwt_secret);
+
+  res.status(200).json({
+    status: 'success',
+    token
+  })
+});
 
 app.get('/auth/google', async (req, res, next) => {
   const code = req.query.code;
 
   console.log(`Code is ${code}`);
 
+  // request user token.
   const response = await getTokens(code); //getTokens  getAuthTokens
+  const { access_token, refresh_token, id_token } = response;
 
-  // console.log('RES IS', response);
+  console.log({ access_token, refresh_token, id_token });
+
+  // get user
+  const user = await getGoogleUser({ access_token, id_token });
+
+  console.log({ user });
+
+  res.redirect(
+    `${process.env.SERVER_ROOT_URI}/home?${querystring.stringify({
+      name: user.given_name,
+    })}`
+  );
+});
+
+app.get('/home', async (req, res, next) => {
+  const { name } = req.query;
+  const path = __dirname + '/public/home.html';
+  const readFile = util.promisify(fs.readFile);
+
+  var home = await readFile(path, 'utf-8');
+  home = home.replace('{{name}}', name);
+
+  res.set('Content-Type', 'text/html');
+
+  res.send(home);
 });
 
 app.get('/auth/google/url', (req, res, next) => {
